fd9121db5bfc90f665c1f780c8b369800040e385
Senglay Pann <senglaypann@gmail.com>
2025-10-19 14:57:09 +0700

    Configure: reconfigure android firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,6 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-     // Add the dependency for the Google services Gradle plugin
-    id("com.android.application")
-    id("com.google.gms.google-services")
 }
 
 dependencies {
