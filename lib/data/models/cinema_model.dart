940758fecd945d7fd7f838efaf401a66161ef86f
Senglay Pann <senglaypann@gmail.com>
2025-11-29 10:29:26 +0700

    Refactor: refactor cinema screen

--- lib/data/models/cinema_model.dart
+++ lib/data/models/cinema_model.dart
@@ -11,6 +11,8 @@ class CinemaModel {
   final List<String> facilities;
   final int hallCount;
 
+  final String imageUrl;
+
   CinemaModel({
     required this.id,
     required this.name,
@@ -21,19 +23,35 @@ class CinemaModel {
     required this.closeHour,
     required this.facilities,
     required this.hallCount,
+    required this.imageUrl,
   });
 
   factory CinemaModel.fromMap(Map<String, dynamic> data, String id) {
+    // Handle location which might be a Map in JSON or GeoPoint in Firestore
+    GeoPoint location;
+    if (data['location'] is Map) {
+      final locMap = data['location'] as Map;
+      location = GeoPoint(
+        (locMap['lat'] as num).toDouble(),
+        (locMap['lng'] as num).toDouble(),
+      );
+    } else if (data['location'] is GeoPoint) {
+      location = data['location'];
+    } else {
+      location = const GeoPoint(0, 0);
+    }
+
     return CinemaModel(
       id: id,
       name: data['name'] ?? '',
-      location: data['location'] ?? const GeoPoint(0, 0),
+      location: location,
       address: data['address'] ?? '',
       city: data['city'] ?? '',
       openHour: data['openHour'] ?? '',
       closeHour: data['closeHour'] ?? '',
       facilities: List<String>.from(data['facilities'] ?? []),
       hallCount: data['hallCount'] ?? 0,
+      imageUrl: data['imageUrl'] ?? '',
     );
   }
 
