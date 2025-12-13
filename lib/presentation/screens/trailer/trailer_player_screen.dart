3c4a973e9533202ab1c7fcc70c954ef21e6bc9c5
Senglay Pann <senglaypann@gmail.com>
2025-12-13 11:05:47 +0700

    Feat: add youtube video player

--- lib/presentation/screens/trailer/trailer_player_screen.dart
+++ lib/presentation/screens/trailer/trailer_player_screen.dart
@@ -1,38 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import '../../widgets/app_scaffold.dart';
 
-class TrailerPlayerScreen extends StatelessWidget {
+class TrailerPlayerScreen extends StatefulWidget {
   final String trailerUrl;
 
   const TrailerPlayerScreen({super.key, required this.trailerUrl});
 
   @override
+  State<TrailerPlayerScreen> createState() => _TrailerPlayerScreenState();
+}
+
+class _TrailerPlayerScreenState extends State<TrailerPlayerScreen> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    final videoId = YoutubePlayer.convertUrlToId(widget.trailerUrl);
+    _controller = YoutubePlayerController(
+      initialVideoId: videoId ?? '',
+      flags: const YoutubePlayerFlags(autoPlay: true, mute: false),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AppScaffold(
-      title: "Trailer",
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.play_circle_outline, size: 100, color: Colors.red),
-            const SizedBox(height: 20),
-            const Text(
-              "Playing Trailer",
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                trailerUrl,
-                style: const TextStyle(color: Colors.white70),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            // TODO: Implement actual video player here using youtube_player_flutter or video_player
-          ],
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller,
+        showVideoProgressIndicator: true,
+        progressIndicatorColor: Colors.red,
+        progressColors: const ProgressBarColors(
+          playedColor: Colors.red,
+          handleColor: Colors.redAccent,
         ),
+        onReady: () {
+          // Player is ready.
+        },
       ),
+      builder: (context, player) {
+        return AppScaffold(
+          title: "Trailer",
+          body: Center(child: player),
+        );
+      },
     );
   }
 }
